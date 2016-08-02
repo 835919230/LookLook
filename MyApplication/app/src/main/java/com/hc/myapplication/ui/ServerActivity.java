6b3981dbf11348100ec4732135a8f10b9525d9e4
何肸 <835919230@qq.com>
2016-08-02 20:30:25 +0800

    ?

--- MyApplication/app/src/main/java/com/hc/myapplication/ui/ServerActivity.java
+++ MyApplication/app/src/main/java/com/hc/myapplication/ui/ServerActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -31,6 +33,8 @@ public class ServerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_server);
         initToolBar();
         getSupportFragmentManager()
